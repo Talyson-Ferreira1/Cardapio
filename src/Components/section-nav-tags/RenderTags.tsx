b5ee09e66159fcb1e8c0e10ddaebe2bda64c494c
talyson <talyson.campos13@gmail.com>
2023-08-19 21:20:48 -0300

    fix: tags button

--- src/Components/section-nav-tags/RenderTags.tsx
+++ src/Components/section-nav-tags/RenderTags.tsx
@@ -3,69 +3,59 @@ import { useEffect, useState } from 'react';
 import { getFirestore, collection, doc, getDoc } from 'firebase/firestore';
 import { app } from '@/services/firebase';
 
-
 type tagsProps = {
   [productName: string]: string;
 }
 
-import './tags-style.css'
+import './tags-style.css';
 
 export default function TagsHeader() {
-
-  const [ tags, setTags] = useState <tagsProps> ()
+  const [tags, setTags] = useState<tagsProps>({});
 
   const fetchTagsInDB = async () => {
     try {
-			const infoDatabase = getFirestore(app);
-			const docCollection = collection(infoDatabase, 'Produtos');
-			const docReference = doc(docCollection, 'Tags');
-			const docSnap = await getDoc(docReference);
-			
-			if(docSnap.exists()) {
-				const data = docSnap.data();
-				setTags(data)
+      const infoDatabase = getFirestore(app);
+      const docCollection = collection(infoDatabase, 'Produtos');
+      const docReference = doc(docCollection, 'Tags');
+      const docSnap = await getDoc(docReference);
 
-			} else {
-				console.log('Documento não encontrado!');
-			}
-
-		} catch (error) {
-			console.error('Erro ao buscar dados:', error);
-
-		}
-
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        setTags(data);
+      } else {
+        console.log('Documento não encontrado!');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+    }
   };
 
-  const filterProducts = ( productName:string, idProduct:string) => {
-    console.log(productName + ":" + idProduct)
+  const filterProducts = (productName: string, idProduct: string) => {
+    console.log(productName + ':' + idProduct);
   };
+
+  useEffect(() => {
+    fetchTagsInDB();
+    let lengthTags = Object.values(tags).length
+    console.log(lengthTags)
+    document.documentElement.style.setProperty('--lengthTags', lengthTags.toString());   
+  }, []);
 
   const renderTags = () => {
-    const buttons = [];
+    return Object.keys(tags).map(productName => {
+      const idProduct = tags[productName];
 
-    for (let product in tags) {
-      let idProduct = tags[product];
-      let productName = product;
-
-      buttons.push(
+      return (
         <button key={productName} onClick={() => filterProducts(productName, idProduct)}>
           {productName}
         </button>
-        
       );
-    }
-
-    return buttons;
+    });
   };
 
-  useEffect(()=>{
-    fetchTagsInDB()
-    
-  },[])
-
-  return(
-    <nav className='tags-shortcuts'>
-        {renderTags()}
+  return (
+    <nav className="tags-shortcuts">
+      {renderTags()}
     </nav>
-  )
+  );
 }
---
+++ src/app/task.tsx
@@ -0,0 +1,8 @@
+/** 
+ * todo: Adicionarum botão de ir no componente search.
+ * todo: Adicionar um espaço inicial ao componente tags.
+ * todo: Salvar as informações no sessionStorage, afim de evitar requisições desnecessárias.
+ * Descobrir uma forma de enviar mensagens no whatsap, integrar com a web.
+ * Descobrir como irá funcionr  a lógica de avaliação do produto.
+ * 
+ * */ 
