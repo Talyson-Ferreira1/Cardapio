4b6fbc2c02090927ad304d00be6a4fb635c22d62
talyson <talyson.campos13@gmail.com>
2023-08-20 13:50:58 -0300

    fix: [error fix  the recent push]

--- src/app/clipboard.tsx
+++ src/app/clipboard.tsx
@@ -1,70 +1,75 @@
-
-
-
 const Meals = {
-        "161141721531273151327618114715": {
-            name: "Panqueca com frango",
-            description: "Panqueca de frango, file de peito sassame, arroz, feijão, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "16114172153127": {
-            name: "Panqueca",
-            description: "Panqueca de frango, arroz, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "6181147152761892015": {
-            name: "Frango frito",
-            description: "Frango frito, arroz, feijão, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "6181147152731513273118145": {
-            name: "Frango com carne",
-            description: "Frango frito, bife de carne de gado, arroz, feijão, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "311814527452771415": {
-            name: "Carne de gado",
-            description: "Bife de carne de gado, arroz, feijão, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "1659245273151327219115": {
-            name: "Peixe com baião",
-            description: "Peixe frito, Baião, batata frita, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "21911527139192015": {
-            name: "Baião misto",
-            description: "Linguiça frita, ovo frito, baião, batata frita, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "3185135274527618114715": {
-            name: "Creme de frango",
-            description: "Creme de frango, arroz, macarrão, batata palha, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        "31851352745276181147152731513276181147152761892015": {
-            name: "Creme de frango com frango frito",
-            description: "Creme de frango, frango frito, arroz, macarrão, tomate, cebola e alface. ",
-            price: 10.00,
-            available: false,
-            stars:5,
-        },
-        
-    }
+  '161141721531273151327618114715': {
+    name: 'Panqueca com frango',
+    description:
+      'Panqueca de frango, file de peito sassame, arroz, feijão, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '16114172153127': {
+    name: 'Panqueca',
+    description:
+      'Panqueca de frango, arroz, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '6181147152761892015': {
+    name: 'Frango frito',
+    description:
+      'Frango frito, arroz, feijão, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '6181147152731513273118145': {
+    name: 'Frango com carne',
+    description:
+      'Frango frito, bife de carne de gado, arroz, feijão, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '311814527452771415': {
+    name: 'Carne de gado',
+    description:
+      'Bife de carne de gado, arroz, feijão, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '1659245273151327219115': {
+    name: 'Peixe com baião',
+    description: 'Peixe frito, Baião, batata frita, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '21911527139192015': {
+    name: 'Baião misto',
+    description:
+      'Linguiça frita, ovo frito, baião, batata frita, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '3185135274527618114715': {
+    name: 'Creme de frango',
+    description:
+      'Creme de frango, arroz, macarrão, batata palha, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+  '31851352745276181147152731513276181147152761892015': {
+    name: 'Creme de frango com frango frito',
+    description:
+      'Creme de frango, frango frito, arroz, macarrão, tomate, cebola e alface. ',
+    price: 10.0,
+    available: false,
+    stars: 5,
+  },
+};
 
+Meals;
